06b7a185b8dbfc3e6a470cc980767da4709c7df6
Ahoonk <ncyahonk@gmail.com>
2026-07-27 11:55:46 +0700

    edit faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = $this->faker;
+
         return [
             'company_id' => null,
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role' => 'admin',
